92bdea6c2c8f712586517a096ac360722917f69f
Marcus Plutowski <achierius@gmail.com>
2018-12-20 16:54:01 -0800

    Implement Node

---
+++ include/node.hpp
@@ -0,0 +1,23 @@
+#ifndef __NP_SUDOKU_NODE
+#define __NP_SUDOKU_NODE
+
+class Node {
+public:
+    Node();
+    Node(unsigned int value);
+
+    ~Node();
+    Node(const Node& to_copy);
+    Node& operator=(const Node& to_copy);
+
+    void empty();
+    void set(unsigned int value);
+
+    unsigned int value();
+    bool determined();
+private:
+    unsigned int _value;
+    bool _determined;
+}
+
+#endif//__NP_SUDOKU_NODE
---
+++ src/node.cpp
@@ -0,0 +1,48 @@
+#include "node.hpp"
+
+Node::Node() {
+    _determined = false;
+}
+
+Node::Node(unsigned int value) {
+    _determined = true;
+    _value = value;
+}
+
+Node::~Node() {
+}
+
+Node::Node(const Node& to_copy) {
+    this._determined = to_copy._determined;
+    if(_determined) {
+        this._value = to_copy._value;
+    }
+}
+
+
+Node& Node::operator=(const Node& to_copy) {
+    this._determined = to_copy._determined;
+    if(_determined) {
+        this._value = to_copy._value;
+    }
+}
+
+void Node::empty() {
+    this._determined = false;
+}
+
+void Node::set(unsigned int value) {
+    this._determined = true;
+    this._value = value;
+}
+
+unsigned int Node::value() { //TODO: Exceptions when called while !_determined
+    if(_determined) {
+        return _value;
+    }
+    return 0;
+}
+
+bool Node::determined() {
+    return _determined;
+}
