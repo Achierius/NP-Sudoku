c82943ad3feda54a84ee9d3bd4dac990b475bcd8
Marcus Plutowski <achierius@gmail.com>
2018-12-20 16:54:11 -0800

    Decapitalize Vector in lin_eq.hpp~

--- include/linear_equation.hpp
+++ include/linear_equation.hpp
@@ -10,21 +10,21 @@
 class LinearEquation {
 public:
     LinearEquation(); //0 = 0
-    LinearEquation(std::Vector<double> coefficients); //coeffs = 0
-    LinearEquation(std::Vector<double> coefficients, double constant);
+    LinearEquation(std::vector<double> coefficients); //coeffs = 0
+    LinearEquation(std::vector<double> coefficients, double constant);
 
     ~LinearEquation();
     LinearEquation(const LinearEquation toCopy);
     LinearEquation& operator=(const LinearEquation toCopy);
     
-    bool validate(std::Vector<double> values);
-    double test(std::Vector<double> values);
+    bool validate(std::vector<double> values);
+    double test(std::vector<double> values);
     LinearEquation substitute(int variable, double value);
-    LinearEquation substitute(std::Vector<int> variables, std::Vector<double> values);
+    LinearEquation substitute(std::vector<int> variables, std::vector<double> values);
 
 private:
     double _constant;
-    std::Vector<double> _coefficients;
+    std::vector<double> _coefficients;
 
 };
 
