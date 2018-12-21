f3f4764666cbab3293725c0f250b9181d1875aa1
Marcus Plutowski <achierius@gmail.com>
2018-12-21 15:31:24 -0800

    Add skeleton for clauses in a boolean eqn
    I think this is fine -- I just don't know
    how I want the 'end-state variables' to be
    like. Right now they're just going to be
    wrappers around a Bit, which I'm not sure if
    I like -- however, it's nice to have the
    undetermined/determined construct there.

---
+++ include/clause.hpp
@@ -0,0 +1,40 @@
+#ifndef __NP_SUDOKU_CLAUSE
+#define __NP_SUDOKU_CLAUSE
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "bit.hpp"
+
+class Clause {
+public:
+  enum class Operator {
+    kIdentity = 0, kNegate,
+    kAnd, kNand,
+    kOr, kNor,
+    kXor, kNxor,
+  };
+
+  Clause();
+  Clause(std::vector<Clause> init, Operator opr);
+  Clause(Clause& init, Operator opr);
+
+  void negate();
+  void setOperator(Operator new_opr);
+  Operator getOperator();
+
+  int numClauses();
+  void addClause(Clause& clause);
+  void insertClause(Clause& clause, int index);
+  Clause& getClause(int index);
+  Clause& removeClause(int index);
+
+  int evalNum(); //0 if evaluateable, else equals the number of variables which remain undetermined
+  Bit evaluate(); //Undetermined if cannot be evaluate
+
+private:
+  Operator operator_gt;
+  std::vector<Clause> clauses_;
+};
+
+#endif//__NP_SUDOKU_CLAUSE
