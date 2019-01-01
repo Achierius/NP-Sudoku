410fd7d08ba903dd9353b97cbfcfaa5a317f274d
Marcus Plutowski <achierius@gmail.com>
2019-01-01 04:54:43 -0800

    Add a handler class for CNFVariables In order to get good efficiency, I don't THINK I want to be iterating over a list of clauses -- that'd be O(n) for every single SAT-solving operation step! Instead, my idea is to have each variable have a set of pointers to all clauses they belong to, so if the SAT solver needs to do something to those clauses (check their value, most likely) it can access them through the variable! Still technically linear time, but the total number of clauses will be more than the number of clauses contained by at least one variable, save for trivial cases.

---
+++ include/cnf_handler.hpp
@@ -0,0 +1,65 @@
+#ifndef __NP_SUDOKU_CNF_HANDLER
+#define __NP_SUDOKU_CNF_HANDLER
+
+#include <vector>
+#include <memory>
+
+#include "cnf_equation.hpp"
+#include "cnf_variable.hpp"
+
+class CNFHandler {
+public:
+  /** Creates a handler with a default-constructed
+   *  CNFVariable.
+   */
+  CNFHandler();
+
+  /** Each handler is supposed to keep a list of clauses
+   *  pointing at this specific CNFVariable, and there's
+   *  only supposed to be one CNFHandler per CNFEquation --
+   *  copying them makes no sense!
+   */
+  CNFHandler(const CNFHandler& to_copy) = delete;
+  CNFHandler& operator=(const CNFHandler& to_copy) = delete;
+  ~CNFHandler();
+
+  //TODO: Move semantics
+
+  /** Adds clause to clauses_ by creating a
+   *  shared_ptr to point to it. Returns false
+   *  if clause is already in clauses_, otherwise
+   *  returns true. Does not add duplicate clauses.
+   */
+  bool addClause(CNFClause& new_clause);
+  /** Removes given clause by checking equality
+   *  with every clause in the list and removing the first
+   *  clause to match.
+   */
+  bool removeClause(CNFClause& old_clause);
+
+  /** Returns the size of clauses_. */
+  int numClauses();
+
+  /** Sets variable_ to new_variable. */
+  void setVariable(CNFVariable new_variable);
+  /** Returns a copy of variable_. */
+  CNFVariable getVariable();
+
+  /** Does this make sense? Is it valid at all? */
+  const std::vector<std::shared_ptr<CNFEquation::CNFClause> >::iterator clauses();
+
+private:
+  /** Variable wrapped by the handler object. */
+  CNFVariable variable_;
+  /** A (not necessarily complete) list of CNFClauses
+   *  containing a pointer to this variable. CNFHandler
+   *  is responsible for all clauses in this object.
+   *  Should contain a complete list of CNFClauses
+   *  containing a pointer to this variable belonging to
+   *  a specific CNFEquation for at least one CNFEquation.
+   *  The pointer-references will remain valid during the
+   *  lifespan of the given CNFEquation. */
+  std::vector<std::shared_ptr<CNFEquation::CNFClause> > clauses_;
+};
+
+#endif//__NP_SUDOKU_CNF_HANDLER
