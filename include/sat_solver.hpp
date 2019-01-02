b08d785425381abb2a4b3d6adab60764bd545384
Marcus Plutowski <achierius@gmail.com>
2019-01-02 01:22:13 -0800

    Refactor sat_solver.hpp to interface with the new cnf_ headers

--- include/sat_solver.hpp
+++ include/sat_solver.hpp
@@ -3,20 +3,27 @@
 
 #include <vector>
 
-#include "clause.hpp"
+#include "cnf_equation.hpp"
+#include "cnf_variable.hpp"
 
 class SatSolver {
 public:
   SatSolver();
+  SatSolver(CNFEquation eqn);
+  SatSolver(std::vector<CNFEquation::CNFClause> clauses);
 
   ~SatSolver();
   SatSolver(const SatSolver& to_copy);
   SatSolver& operator=(const SatSolver& to_copy);
 
+  void setEquation(CNFEquation eqn);
+  void setEquation(std::vector<CNFEquation::CNFClause> clauses);
+
   virtual bool satisfiable() = 0;
   virtual std::vector<bool> solve() = 0;
 
 private:
+  CNFEquation equation_;
 };
 
 #endif//__NP_SUDOKU_SAT_SOLVER
