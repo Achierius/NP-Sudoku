49c2cd066af407aec641bc96f44f88c1790e53b5
Marcus Plutowski <achierius@gmail.com>
2019-01-07 01:15:04 -0800

    Finish templated cnf_variable

---
+++ src/cnf_variable.cc
@@ -0,0 +1,14 @@
+#include "cnf_variable.hpp"
+
+CNFVariable::CNFVariable() {
+
+}
+
+CNFVariable::CNFVariable(T identifier_) {
+
+}
+
+CNFVariable::CNFVariable<T>(T identifier_, bool initValue) {
+
+}
+
