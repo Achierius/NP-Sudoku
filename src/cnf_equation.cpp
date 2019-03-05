b2be7be8e5d359163f083081cd9a7978d454b804
Marcus Plutowski <achierius@gmail.com>
2019-03-05 05:14:02 -0800

    Fix skeleton file for .cpp

--- src/cnf_equation.cpp
+++ src/cnf_equation.cpp
@@ -12,25 +12,36 @@ CNFEquation& CNFEquation::operator=(const CNFEquation& to_copy) {
 }
 CNFEquation::~CNFEquation() = default;
 
-CNFEquation::CNFEquation(const std::initializer_list<cnf_clause_t>& clauses) {
+CNFEquation::CNFEquation(const std::initializer_list<clause_t>& clauses) {
 
 }
 
-CNFEquation::CNFEquation(const std::initializer_list<cnf_clause_t>& clauses,
+CNFEquation::CNFEquation(const std::initializer_list<clause_t>& clauses,
             std::initializer_list<value_t>) {
 
 }
 
-void CNFEquation::setVariable(variable_t variable, value_t new_value) {
+void CNFEquation::setVariable(variable_t variable, value_t new_val) {
 
 }
-void CNFEquation::setRange(variable_t min, variable_t max, value_t new_value) {
+void CNFEquation::setRange(variable_t min, variable_t max, value_t new_val) {
 
 }
-void CNFEquation::setAll(value_t new_value) {
+void CNFEquation::setAll(value_t new_val) {
 
 }
 
 CNFEquation::value_t CNFEquation::getVariable(variable_t variable) const {
+
+}
+
+void CNFEquation::addClause(const clause_t& new_clause) {
+
+}
+void CNFEquation::removeClause(const clause_t& clause_to_delete) {
+
+}
+
+std::vector<std::shared_ptr<const CNFEquation::clause_t> > CNFEquation::varClauses(variable_t variable) {
 
 }
