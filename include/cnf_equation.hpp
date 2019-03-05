67f8f454ba620965107c3347ac207c4e810e9679
Marcus Plutowski <achierius@gmail.com>
2019-03-05 05:09:24 -0800

    Simplify cnf_equation type structure

--- include/cnf_equation.hpp
+++ include/cnf_equation.hpp
@@ -14,17 +14,17 @@ class CNFEquation {
 public:
   using value_t                   = std::optional<bool>;
   using variable_t                = uint64_t;
+  using clause_t = std::vector<const std::pair<variable_t, bool> >;
   const static variable_t CNF_MAX = UINT64_MAX;
-  using cnf_clause_t              = std::vector<const std::pair<variable_t, bool> >;
-  using cnf_variable_t            = std::pair<value_t, std::vector<std::shared_ptr<cnf_clause_t> > >;
+  const static variable_t CNF_MIN = 0;
 
   CNFEquation();
   CNFEquation(const CNFEquation& to_copy);
   CNFEquation& CNFEquation::operator=(const CNFEquation& to_copy);
   ~CNFEquation();
 
-  CNFEquation(const std::initializer_list<cnf_clause_t>& clauses);
-  CNFEquation(const std::initializer_list<cnf_clause_t>& clauses,
+  CNFEquation(const std::initializer_list<clause_t>& clauses);
+  CNFEquation(const std::initializer_list<clause_t>& clauses,
               std::initializer_list<value_t>); //TODO: Make sure you correctly parse the variable's clauses
 
   void setVariable(variable_t variable, value_t new_value);
@@ -35,17 +35,18 @@ public:
 
   variable_t maxVariable(); //Returns the index of the maximum variable currently included in a clause in clauses_
 
-  void addClause(const cnf_clause_t& new_clause);
-  void removeClause(const cnf_clause_t& clause_to_delete);
+  void addClause(const clause_t& new_clause);
+  void removeClause(const clause_t& clause_to_delete);
 
-  std::vector<std::shared_ptr<cnf_clause_t> > varClauses(variable_t variable);
+  std::vector<std::shared_ptr<clause_t> > varClauses(variable_t variable);
 
 private:
   void reparseCReferences(); //Recalculates all vectors of shared_ptr<snf_clause_t> in variables_
   void reparseMaxVariable(); //Recalculates the correct value of max_variable
 
-  std::list<std::shared_ptr<cnf_clause_t> > clauses_;
-  std::array<cnf_variable_t, CNF_MAX> variables_;
+  std::list<std::shared_ptr<clause_t> > clauses_;
+  std::array<value_t, CNF_MAX> variables_;
+  std::array<std::vector<std::shared_ptr<clause_t> >, CNF_MAX> references_;
   variable_t max_variable;
 };
 
