9ede1583e7c6434be5d88b8a56cf60fe4ae35acf
Marcus Plutowski <achierius@gmail.com>
2019-03-05 06:05:31 -0800

    Add faulty implementations for add/removing clauses
    Idk why there's a red squiggly on a single period
    but it's 6am and I have class at noon:30
    Otherwise I think it's fine? A bit funky and I'll
    want to look through the logic again but there's
    nothing obviously bad.

--- src/cnf_equation.cpp
+++ src/cnf_equation.cpp
@@ -1,4 +1,5 @@
 #include "cnf_equation.hpp"
+#include <algorithm>
 
 CNFEquation::CNFEquation() {
 
@@ -38,10 +39,25 @@ CNFEquation::value_t CNFEquation::getVariable(variable_t variable) const {
 }
 
 void CNFEquation::addClause(const clause_t& new_clause) {
-
+  clauses_.push_back(std::make_shared<clause_t>(new_clause));
+  for(auto i : new_clause) {
+    references_[i.first].push_back(std::make_shared<clause_t>(new_clause));
+  }
 }
 void CNFEquation::removeClause(const clause_t& clause_to_delete) {
-
+  for(auto itr = clauses_.begin(); itr != clauses_.end(); itr++) {
+    if((**itr) == clause_to_delete) {
+      clauses_.erase(itr);
+      break;
+    }
+  }
+  for(auto i : clause_to_delete) {
+    references_[i.first].erase(std::remove_if(references_.begin(), references_.end(),
+                   [=](std::shared_ptr<clause_t> arg){return *arg == clause_to_delete;}));
+    if(i.first == maxVariable() && references_[i.first].size() == 0) {
+      reparseMaxVariable();
+    }
+  }
 }
 
 std::vector<std::shared_ptr<const CNFEquation::clause_t> > CNFEquation::varClauses(variable_t variable) {
